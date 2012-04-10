903bb3b3bd84540a5439ad1cdf0583cade830945
Paceaux <frank.m.taylor08@gmail.com>
2012-04-09 22:24:14 -0400

    Jumping from 1.0 to 2.0. Forminator has been turned into a jQuery plugin. See readme for details

--- form_protector.js
+++ form_protector.js
@@ -1,31 +1,52 @@
-//Form Protector: Forminator T100 series
+/*!
+* Forminator T100: Protect all your form inputs from bad internet connections and an angry John Connor
+* @requires Jquery v1.4 or above
+*
+* Copyright (c) 2012 Frank M. Taylor
+* Dual licensed under MIT and GPL licenses 
+* http://opensource.org/licenses/MIT
+* http://www.gnu.org/licenses/gpl.html
+* version 0.1.0
+*/
+/*
+*   USAGE
+*   1. apply .forminator() to any form
+*   2. include optional arguments for which selectors you want, whether we store on name or id, and clear button
+*/
+/*   VERSIONS
+*       0.1.0 - First working version
+*       0.2.0 - Turned into a jQuery plugin
+*/
+;(function ( $ ) {
+$.fn.forminator = function(storedFields, storageKey, clearButton) {
+    form = this;    
 // CAPTURE FORM DATA
 //take whatever the input field is, put its value in storage on keyup
-$('input, datalist, keygen, optgroup, select, textarea').keyup(function(event){ 
-    var name;
-    var value;
-    var name = $(this).attr("name");
+$(form).children('input, datalist, keygen, optgroup, select, textarea').keyup(function(event){ 
+    var name = $(form).attr("id")+"-" + $(this).attr(storageKey);
     var value = $(this).val();
     localStorage.setItem(name, value);
     currentval = $(this).val();
 });
-
 //use this for the input slider - since it doesn't use the keyboard
-$('[type="range"]').mousemove(function(event){
- var name;
-    var value;
-    var name = $(this).attr("name");
+$(form).children('[type="range"]').mousemove(function(event){
+    var name = $(this).attr(storageKey);
     var value = $(this).val();
     localStorage.setItem(name, value);
     currentval = $(this).val();
 });
-
 //load the values into input forms
-$('input,textarea,select, datalist, keygen, optgroup').each(function(event) {
-var fieldkey = $(this).attr("name");
-var fieldval = localStorage.getItem(fieldkey);
-$(this).val(fieldval);
+$(form).children('input,textarea,select, datalist, keygen, optgroup').each(function(event) {
+    var fieldkey = $(form).attr("id") + "-" + $(this).attr(storageKey);
+    var fieldval = localStorage.getItem(fieldkey);
+    $(this).val(fieldval);
+});
+$(clearButton).click(function(event){
+    localStorage.clear();    
 });
 //Copyright 2011. All rights reserved.
 //rights are also reserved for Alexandar Kaupanin to piss off for his article on smashing magazine 5 days after I published mine on my blog. 
+};
+})(jQuery);
 
+$('#elformo').forminator('input', 'name', '#clearIt');
--- form_protector_demo.js
+++ form_protector_demo.js
@@ -1,38 +1,3 @@
-// CAPTURE FORM DATA
-//take whatever the input field is, put its value in storage on keyup
-$('input, datalist, keygen, optgroup, select, textarea').keyup(function(event){ 
-    var name;
-    var value;
-    var name = $(this).attr("name");
-    var value = $(this).val();
-    localStorage.setItem(name, value);
-    currentval = $(this).val();
-    
-    //demo stuff only
-    outputs = $('output[name="form output"]').html;
-    $('output[name="form output"]').html("<p>" + currentval + "</p>" );
-
-});
-
-//use this for the input slider - since it doesn't use the keyboard
-$('[type="range"]').mousemove(function(event){
- var name;
-    var value;
-    var name = $(this).attr("name");
-    var value = $(this).val();
-    localStorage.setItem(name, value);
-    currentval = $(this).val();
-    //demo stuff only
-    outputs = $('output').html;
-    $('output[name="form output"]').html("<p>" + currentval + "</p>" );
-});
-
-//load the values into input forms
-$('input,textarea,select, datalist, keygen, optgroup').each(function(event) {
-var fieldkey = $(this).attr("name");
-var fieldval = localStorage.getItem(fieldkey);
-$(this).val(fieldval);
-});
 
 
 //ONLY FOR DEMO - REMOVE FROM PRODUCTION
